bf621ca2313c47e4c6b62cc8751993b650734553
Nate <nate@natesoares.com>
2010-11-27 19:55:22 -0500

    Merge with Nate

--- MyBot.py
+++ MyBot.py
@@ -1,6 +1,11 @@
-from universe import Universe
 from planet import Planet
+from planetwars import universe
 from planetwars import Game
 from bot import Bot
+
+class Universe(universe.Universe):
+    quota = float('infinity')
+    idlers = 0
+    marks = set()
 
 Game(Bot, Universe, Planet)
--- universe.py
+++
@@ -1,9 +0,0 @@
-from logging import getLogger
-log = getLogger('nate')
-
-from planetwars import universe
-
-class Universe(universe.Universe):
-    quota = float('infinity')
-    idlers = 0
-    marks = set()
